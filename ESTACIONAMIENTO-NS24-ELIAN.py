4203e2c315289e3eb26635c97373b867e76e9af4
Elian Olart <53961725+JernelOlart@users.noreply.github.com>
2024-06-22 12:16:11 -0600

    Update ESTACIONAMIENTO-NS24-ELIAN.py

--- ESTACIONAMIENTO-NS24-ELIAN.py
+++ ESTACIONAMIENTO-NS24-ELIAN.py
@@ -37,11 +37,17 @@ def crear_plano_estacionamiento():
         ])
         ax.add_patch(plt.Polygon(rectangulo, closed=True, fill=None, edgecolor='b'))
 
+    # Agregar ángulo de inclinación al gráfico
+    ax.text(0.5, 1.15, f'Ángulo de inclinación: {angulo_inclinacion}°', horizontalalignment='center', verticalalignment='center', transform=ax.transAxes, fontsize=9)
+
     # Agregar etiquetas y mostrar el plano
     ax.set_xlabel('Largo (m)')
     ax.set_ylabel('Alto (m)')
-    ax.set_title('Plano del Estacionamiento')
+    ax.set_title('Plano del Estacionamiento Núcleo Sepec v2', pad=40)  # Aumenta el espacio del título desde el borde superior
+    ax.text(0.5, 0.5, 'soportevh@nucleosepec.com.mx', horizontalalignment='center', verticalalignment='center', transform=ax.transAxes, fontsize=8, color='gray')
+  
     plt.grid(True)
+    plt.tight_layout()  # Ajusta automáticamente el espaciado de la figura
     plt.savefig('plano_estacionamiento.png')  # Guardar la gráfica en un archivo
     plt.show()
 
